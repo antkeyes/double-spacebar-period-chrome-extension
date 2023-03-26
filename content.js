a41e7071f370a4d7219ac961a17934ca373ffda1
antKeyes <keyes.anthony@gmail.com>
2023-03-26 19:10:23 -0400

    detect double space bar presses * detect double space bar presses * insert period followed by a space

---
+++ content.js
@@ -0,0 +1,14 @@
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'Space' && event.target.tagName.toLowerCase() === 'textarea') {
+      let textarea = event.target;
+      let value = textarea.value;
+      let start = textarea.selectionStart;
+  
+      if (start >= 1 && value.charAt(start - 1) === ' ' && value.charAt(start - 2) !== '.') {
+        event.preventDefault();
+        textarea.value = value.substring(0, start - 1) + '. ' + value.substring(start);
+        textarea.selectionStart = textarea.selectionEnd = start + 1;
+      }
+    }
+  });
+  
